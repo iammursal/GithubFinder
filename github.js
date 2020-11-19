190cf7443ef5169abb7967b6f7553ab09b72f1ff
Mursal Akhtar Ansari <52567409+iammursal@users.noreply.github.com>
2020-11-19 11:44:50 +0530

    Update github.js

--- github.js
+++ github.js
@@ -8,10 +8,10 @@ class Github {
     async getUser (user) {
         // const profileRes = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
         const profileRes = await fetch(`https://api.github.com/users/${user}`, {
-            headers: { authorization: "token 899f4ff8107a23b7c958583ef42eca3cd9fd4e48" }
+            headers: { authorization: "899f4ff8107a23b7c958583ef42eca3cd9fd4e48" }
          });
         const repoRes = await fetch(`https://api.github.com/users/${user}/repos?per_page=5&sort_by=created:asc`, { headers: {
-         authorization: "toker 899f4ff8107a23b7c958583ef42eca3cd9fd4e48"
+         authorization: "899f4ff8107a23b7c958583ef42eca3cd9fd4e48"
         }});
         const profile = await profileRes.json();
         const repo = await repoRes.json();
